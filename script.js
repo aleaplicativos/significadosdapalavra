68c6f04337611a33fcec7ba5b30f9d67fb373d90
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-06-17 15:46:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Função para buscar significado da palavra
+async function searchWord() {
+  const input = document.getElementById('search-input').value;
+  const apiUrl = `https://api.dicionario-aberto.net/word/${input}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    // Exibir significado no elemento 'search-result'
+    const searchResult = document.getElementById('search-result');
+    searchResult.innerHTML = '';
+
+    data.forEach(entry => {
+      const word = entry.word;
+      const definition = entry.xml;
+
+      const wordElement = document.createElement('h3');
+      wordElement.textContent = word;
+
+      const definitionElement = document.createElement('p');
+      definitionElement.innerHTML = definition;
+
+      searchResult.appendChild(wordElement);
+      searchResult.appendChild(definitionElement);
+    });
+  } catch (error) {
+    console.error('Erro ao buscar palavra:', error);
+  }
+}
+
+// Event listener para o formulário de busca
+const searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  searchWord();
+});
